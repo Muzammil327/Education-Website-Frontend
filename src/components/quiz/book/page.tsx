d15f9bc61da0ccbe3547b4780acaadc65cab96d4
Muzammil327 <mmuzammiloff327@gmail.com>
2024-02-21 16:44:32 +0500

    first commit

--- src/components/quiz/book/page.tsx
+++ src/components/quiz/book/page.tsx
@@ -13,6 +13,9 @@ export default function QuizBookSection() {
       try {
         let baseUrl;
 
+
+
+        
         if (process.env.NODE_ENV === 'development') {
           baseUrl = 'http://localhost:8000';
         } else {
